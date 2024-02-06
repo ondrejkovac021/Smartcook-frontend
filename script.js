be6ec4373b26831fe2956b600a0a482fa64d2e60
Ondřej Kováč <96345636+ondrejkovac021@users.noreply.github.com>
2024-02-06 17:23:11 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function show(thingy){
-    const menu = document.getElementById(thingy)
-    if(menu.style.display == "none"){
-        menu.style.display = "block"
-    }else{
-        menu.style.display = "none"
-    }
-}
